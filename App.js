43d86ff24886fc2ff960c92989a9c3b3a5c981e1
Robetson <80962857+betthink@users.noreply.github.com>
2022-02-26 22:37:12 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import HomePage from './screen/HomePage';
+import DataProfile from './screen/DataProfile';
+import Pencapaian from './screen/Pencapaian';
+
+const App = () => {
+  return (
+    <View>
+      {/* <HomePage /> */}
+      {/* <DataProfile /> */}
+      <Pencapaian />
+    </View>
+  );
+};
+
+export default App;
